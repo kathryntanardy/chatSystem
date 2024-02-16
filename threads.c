1427f95bae533d50ade4e01f782d2d59732b84af
kathryntanardy <ktanardy@gmail.com>
2024-02-16 15:07:58 -0800

    Screen and Receive Implementation

--- threads.c
+++ threads.c
@@ -14,6 +14,7 @@
 
 static pthread_t keyboardThreadPID;
 static pthread_t ReceiverThreadPID;
+static pthread_t ScreenThreadPID;
 
 static pthread_cond_t s_receiveCondVar = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t s_receiveMutexVar = PTHREAD_MUTEX_INITIALIZER;
@@ -22,14 +23,54 @@ static List * receiveList;
 
 static int socketDescriptor;
 
-void * keyboardThread();
-void Keyboard_init();
-void Keyboard_shutDown();
-void Screen_init();
-void Screen_shutDown();
-void Send_init();
-void Send_shutDown();
-void * receiveThread(){
+static void * keyboardThread();
+static void Keyboard_init();
+static void Keyboard_shutDown();
+
+static void freeNode(void* pItem){
+    pItem = NULL;
+}
+
+static void * Screenthread(){
+    printf("Enter message: ");
+    char * messagePointer; 
+
+    while(1){
+        pthread_mutex_lock(&s_receiveMutexVar);
+        {
+            pthread_cond_wait(&s_receiveCondVar, &s_receiveMutexVar);
+            List_first(receiveList);
+            messagePointer = List_remove(receiveList);
+            printf("%s\n", messagePointer);
+            printf("Enter message: ");
+            free(messagePointer);
+        }
+        pthread_mutex_unlock(&s_receiveMutexVar);
+    }
+   
+    return NULL;
+}
+
+static void Screen_init(){
+    pthread_create(
+        &ScreenThreadPID,
+        NULL,
+        Screenthread,
+        NULL
+    );
+}
+
+static void Screen_shutDown(){
+    pthread_cancel(ScreenThreadPID);
+
+    pthread_join(ScreenThreadPID, NULL);
+}
+
+static void Send_init();
+
+static void Send_shutDown();
+
+static void * receiveThread(){
     struct sockaddr_in addr;
 
     addr.sin_family = AF_INET;
@@ -45,14 +86,19 @@ void * receiveThread(){
         struct sockaddr_in sinRemote;
         unsigned int sin_len = sizeof(sinRemote);
         char messageRx[MSG_MAX_LENGTH];
-        recvfrom(socketDescriptor, messageRx, MSG_MAX_LENGTH,
+        int bytesRx = recvfrom(socketDescriptor, messageRx, MSG_MAX_LENGTH,
         0, (struct sockaddr *) &sinRemote, &sin_len);
-      
+        int terminateldx = (bytesRx < MSG_MAX_LENGTH? bytesRx : MSG_MAX_LENGTH - 1);
+        messageRx[terminateldx] = 0;
+        int incMe = atoi(messageRx);
 
         pthread_mutex_lock(&s_receiveMutexVar);
         {
-
-            pthread_mutex_signal(&s_receiveCondVar);
+            char * message = malloc(MSG_MAX_LENGTH);
+            strncpy(message, messageRx, terminateldx);
+            message[terminateldx] = 0;
+            List_append(receiveList, message);
+            pthread_cond_signal(&s_receiveCondVar);
         }
         pthread_mutex_unlock(&s_receiveMutexVar);
 
@@ -62,7 +108,7 @@ void * receiveThread(){
 }
 
 
-void Receive_init(){
+static void Receive_init(){
     pthread_create(
         &ReceiverThreadPID,
         NULL,
@@ -70,9 +116,23 @@ void Receive_init(){
         NULL
     );
 }
-void Receive_shutDown();
+static void Receive_shutDown(){
+    pthread_cancel(ReceiverThreadPID);
+
+    pthread_join(ReceiverThreadPID, NULL);
+}
 
 void systemInit(){
     receiveList = List_create();
+
+    Receive_init();
+    Screen_init();
     
+}
+
+void systemShutDown(){
+    Screen_shutDown();
+    Receive_shutDown();
+
+    List_free(receiveList, freeNode);
 }
